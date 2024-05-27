a18f5a5078237c7a182703adb2ff5e18b7727b86
iamtalib13 <talibsheikh16@gmail.com>
2024-05-27 16:01:50 +0530

    added updated

--- sahayog_docs/circulars/doctype/circular/circular.js
+++ sahayog_docs/circulars/doctype/circular/circular.js
@@ -3,52 +3,105 @@
 
 frappe.ui.form.on("Circular", {
   before_save: function (frm) {
+    if (frm.is_new()) {
+      if (
+        frappe.user.has_role("Circular Manager") ||
+        frappe.user.has_role("System Manager")
+      ) {
+        console.log("admin");
+
+        let file = frm.doc.circular_doc;
+        console.log("file-", file);
+
+        // Define a regular expression to extract the required components
+        let regex =
+          /Circular No\. (\d+) \((\d{4}-\d{2})\) (.+) Date (\d{2}-\d{2}-\d{4})\.pdf/;
+        let match = file.match(regex);
+
+        if (match) {
+          let id = `Circular No. ${match[1]}`;
+          let year = match[2];
+          let fileName = match[3];
+          let date = match[4];
+
+          console.log("ID:", id);
+          console.log("Year:", year);
+          console.log("File Name:", fileName);
+          console.log("Date:", date);
+
+          // Set the extracted date to the form field and refresh the field
+          // frm.set_value("date", date);
+          // frm.refresh_field("date");
+
+          // Optionally, set other fields as well if needed
+          frm.set_value("circular_id", id);
+          frm.refresh_field("circular_id");
+
+          frm.set_value("year", year);
+          frm.refresh_field("year");
+
+          frm.set_value("circular_name", fileName);
+          frm.refresh_field("circular_name");
+
+          // You can now use these variables as needed
+        } else {
+          console.log("Filename does not match the expected pattern");
+        }
+      }
+    }
+  },
+  refresh: function (frm) {
+    if (frm.is_new()) {
+      frm.set_df_property("year", "read_only", 1);
+    } else if (!frm.is_new()) {
+      frm.set_df_property("circular_doc", "read_only", 1);
+      frm.set_df_property("year", "read_only", 0);
+      frm.set_df_property("circular_name", "read_only", 0);
+      frm.set_df_property("circular_id", "read_only", 0);
+    }
+  },
+
+  circular_doc: function (frm) {},
+  upload_doc: function (frm) {
     if (
       frappe.user.has_role("Circular Manager") ||
       frappe.user.has_role("System Manager")
     ) {
-      console.log("admin");
-
-      let file = frm.doc.circular_doc;
-      console.log("file-", file);
-
-      // Define a regular expression to extract the required components
-      let regex =
-        /Circular No\. (\d+) \((\d{4}-\d{2})\) (.+) Date (\d{2}-\d{2}-\d{4})\.pdf/;
-      let match = file.match(regex);
-
-      if (match) {
-        let id = `Circular No. ${match[1]}`;
-        let year = match[2];
-        let fileName = match[3];
-        let date = match[4];
-
-        console.log("ID:", id);
-        console.log("Year:", year);
-        console.log("File Name:", fileName);
-        console.log("Date:", date);
-
-        // Set the extracted date to the form field and refresh the field
-        frm.set_value("date", date);
-        frm.refresh_field("date");
-
-        // Optionally, set other fields as well if needed
-        frm.set_value("circular_id", id);
-        frm.refresh_field("circular_id");
-
-        frm.set_value("year", year);
-        frm.refresh_field("year");
-
-        frm.set_value("circular_name", fileName);
-        frm.refresh_field("circular_name");
-
-        // You can now use these variables as needed
-      } else {
-        console.log("Filename does not match the expected pattern");
-      }
+      uploadFile(frm);
     }
   },
-  refresh: function (frm) {},
-
-  circular_doc: function (frm) {},
 });
+function uploadFile(frm) {
+  new frappe.ui.FileUploader({
+    method: "sahayog_docs.circulars.doctype.circular.circular.capture",
+    make_attachments_public: false,
+    dialog_title: "Upload Circular File",
+    disable_file_browser: false,
+    allow_link: false,
+    frm: frm,
+    restrictions: {
+      allowed_file_types: [".pdf"],
+    },
+    on_success(file, response) {
+      frappe.msgprint(`Server response: ${response.message}`, "Message"); // Log the response for debugging
+
+      frappe.show_alert(
+        {
+          message: __("File uploaded."),
+          indicator: "green",
+        },
+        5
+      );
+
+      frm.refresh();
+    },
+    on_error(error) {
+      console.error(error);
+      frappe.msgprint({
+        title: __("Error"),
+        indicator: "red",
+        message: __("There was an error while uploading the file."),
+      });
+    },
+  });
+}
--- sahayog_docs/circulars/doctype/circular/circular.py
+++ sahayog_docs/circulars/doctype/circular/circular.py
@@ -1,28 +1,48 @@
 # Copyright (c) 2024, Talib Sheikh and contributors
 # For license information, please see license.txt
 import re
+import os
 import frappe
 from frappe.model.document import Document
 
 class Circular(Document):
-	pass
-def before_insert(self):
-        file = self.circular_doc
-        if file:
-            # Define a regular expression to extract the required components
-            regex = r'Circular No\. (\d+) \((\d{4}-\d{2})\) (.+) Date (\d{2}-\d{2}-\d{4})\.pdf'
-            match = re.match(regex, file)
+    pass
 
-            if match:
-                id = f'Circular No. {match.group(1)}'
-                year = match.group(2)
-                file_name = match.group(3)
-                date = match.group(4)
+@frappe.whitelist()
+def capture(file=None):
+    file_content = frappe.local.uploaded_file
+    file_name_old = frappe.local.uploaded_filename
 
-                self.date = date
-                self.circular_id = id
-                self.year = year
-                self.circular_name = file_name
-                self.name=file_name
-            else:
-                frappe.logger().debug('Filename does not match the expected pattern')
+    # Extract details from the file name (assume format "year_circular_id_circular_name.pdf")
+    match = re.match(r"(\d{4})_(\d+)_([^\.]+)\.pdf", file_name_old)
+    if not match:
+        return {"message": "Error: Invalid file name format. Expected 'year_circular_id_circular_name.pdf'."}
+
+    year, circular_id, circular_name = match.groups()
+
+    # Construct the folder path based on the year
+    folder_path = f"private/year/{year}"
+    os.makedirs(os.path.join(frappe.get_site_path(), folder_path), exist_ok=True)
+
+    # Construct the file path
+    file_path = os.path.join(folder_path, file_name_old)
+
+    # Write the file content to the specified path
+    with open(os.path.join(frappe.get_site_path(), file_path), "wb") as file:
+        file.write(file_content)
+
+    # Construct the file URL
+    sys_file_path = f"/{file_path}"
+
+    # Create a new File document in Frappe
+    file_doc = frappe.new_doc("File")
+    file_doc.file_name = file_name_old
+    file_doc.file_url = sys_file_path
+    file_doc.attached_to_name = frappe.get_request_header("Referer").split("/")[-1]
+    file_doc.folder = "Home"
+    file_doc.attached_to_doctype = "Circular"
+    file_doc.attached_to_field = 'upload_image'
+    file_doc.insert()
+
+    # Return response
+    return {"message": "File uploaded successfully.", "file_url": sys_file_path}
